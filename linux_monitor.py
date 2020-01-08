df958e45c84d53142f38e222556f9dadbf02f841
Sean Thames <thames.sean@gmail.com>
2020-01-08 10:17:53 -0500

    Added code to linux_monitor for memory usage and uptime

--- linux_monitor.py
+++ linux_monitor.py
@@ -1,13 +1,29 @@
 import psutil
+from datetime import timedelta
 
 def getCPU():
     return psutil.cpu_percent(interval=0.1)
     
 def getMEM():
-    return psutil.virtual_memory()
+    virt_mem = psutil.virtual_memory().used
+    virt_mem = virt_mem / 1024 # Converts to KiB
+    virt_mem = virt_mem / 1024 # Converts to MiB
+    
+    return round(virt_mem, 2)
 
+def uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+        uptime_string = str(timedelta(seconds = uptime_seconds))
+    
+    return uptime_string[:uptime_string.index('.')]
+    
 def main():
-    print("linux monitor")
+    mem = getMEM()
+    cpu = getCPU()
+    up = uptime()
+
+    print("CPU - {}%\tMEM - {}MiB\tUP - {}".format(cpu, mem, up))
     
 
 if __name__ == "__main__":
